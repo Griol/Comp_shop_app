4645d0facc9d13e7dfb4ac8d6cd46165721e7fec
Griol <voytenko02@mail.ru>
2023-11-24 00:07:50 +0300

    validates
    Добавлен метод validates(length,presence,format)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :fname, presence: true, length: {maximum: 100}
+    validates :lname, presence: true, length: {maximum: 100}
+    validates :email, presence: true, length: {maximum: 150}
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
