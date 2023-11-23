1cc5d01c0e3afd3ba0083fa760600c41ad534a9f
Griol <voytenko02@mail.ru>
2023-11-23 22:38:02 +0300

    Поменял старт.страницу

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "Hello world!"
-    end
-    def bye
-        render html: "Пока пока"
-    end
 
-    
 end
