790ae60a9d5b382643c0f2a4424f23d881940a1f
Prohor Kovalchuk <prohor.stud@yandex.ru>
2023-11-24 09:12:58 +0300

    Модель Devise заработала

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_185750) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_223436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "fname"
     t.string "lname"
-    t.string "email"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
