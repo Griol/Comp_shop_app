b6b85d4bd7003eb9f0774d230a5fb88d6e4a7ca9
Griol <voytenko02@mail.ru>
2024-01-08 11:35:07 +0300

    Создание товара
    Добавлена модель и контроллер для товара

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => "users/registrations"
-  }
+  #devise_for :users, :controllers => {
+  #  :registrations => "users/registrations"
+  #}
+#
+  #root to: 'home#index'
 
-  root to: 'home#index'
+  root "product#index"
+
+  get "/product", to: "product#index"
+
 end
